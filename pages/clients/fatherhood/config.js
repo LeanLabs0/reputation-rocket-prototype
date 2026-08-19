2989e4a8b1f8de8a88dae8fadfa22cd865ea6b5d
Edward Hayford Addo <edwardaddo77@gmail.com>
2026-08-19 10:27:47 +0000

    Update: NFI RR emails

--- pages/clients/fatherhood/config.js
+++ pages/clients/fatherhood/config.js
@@ -20,7 +20,7 @@ window.CLIENT_CONFIG = {
   thankYouRedirectDelayMs: 120000,
   allowedRedirectHosts: [],
   supportEmail: "info@fatherhood.org",
-  notifyEmails: ["mbyers@fatherhood.org","edward@lean-labs.com"],
+  notifyEmails: ["mbyers@fatherhood.org","edward@lean-labs.com","jonathan@lean-labs.com"],
   slackChannel: "",
   slackThreadPositive: "",
   slackThreadNegative: "",
